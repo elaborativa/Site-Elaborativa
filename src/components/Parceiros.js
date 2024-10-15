47ff7de4b70148d55e63c1434b8d2d11e7cc28e9
Vélmer Oliveira <velmer.odon@trio.email>
2024-10-15 12:01:05 -0300

    feat: replace lara with sesi and senai in the Parceiros section

--- src/components/Parceiros.js
+++ src/components/Parceiros.js
@@ -1,32 +1,52 @@
-import styles from './Parceiros.module.css'
+import styles from './Parceiros.module.css';
 
-import lara from '../imgs/parceiros/lara.jpeg'
-import sebrae from '../imgs/parceiros/Sebrae.png'
-import paqtg from '../imgs/parceiros/paqtg.jpg'
+import sesi from '../imgs/parceiros/sesi.png';
+import senai from '../imgs/parceiros/senai.png';
+import sebrae from '../imgs/parceiros/Sebrae.png';
+import paqtg from '../imgs/parceiros/paqtg.jpg';
 
 function Parceiros() {
-    return (
-        <div>
-            <h1 className={styles.h1Parceiros} id='parcerias'>PARCERIAS</h1>
-            <div className={styles.contentParceiros}>
-                <a href='https://www.instagram.com/lara.ifpb/' className={styles.aLara}>
-                    <div className={styles.laraBranco}>
-                        <img src={lara} id={styles.imgLara} />
-                    </div>
-                </a>
-                <a href='https://sebrae.com.br/sites/PortalSebrae/' className={styles.aSebrae}>
-                    <div className={styles.sebraeBranco}>
-                        <img src={sebrae} id={styles.imgSebrae} />
-                    </div>
-                </a>
-                <a href='https://www.paqtc.org.br/' className={styles.aPaqtg}>
-                    <div className={styles.paqtgBranco}>
-                        <img src={paqtg} id={styles.imgpaqtg} />
-                    </div>
-                </a>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className={styles.h1Parceiros} id="parcerias">
+        PARCERIAS
+      </h1>
+      <div className={styles.contentParceiros}>
+        <a
+          href="https://sesieducacao.com.br/publico/"
+          className={styles.parceiroContainer}
+        >
+          <div className={styles.parceiroImagemContainer}>
+            <img src={sesi} id={styles.imgSebrae} />
+          </div>
+        </a>
+        <a
+          href="https://fiepb.com.br/senai/"
+          className={styles.parceiroContainer}
+        >
+          <div className={styles.parceiroImagemContainer}>
+            <img src={senai} id={styles.imgSebrae} />
+          </div>
+        </a>
+        <a
+          href="https://sebrae.com.br/sites/PortalSebrae/"
+          className={styles.parceiroContainer}
+        >
+          <div className={styles.parceiroImagemContainer}>
+            <img src={sebrae} id={styles.imgSebrae} />
+          </div>
+        </a>
+        <a
+          href="https://www.paqtc.org.br/"
+          className={styles.parceiroContainer}
+        >
+          <div className={styles.parceiroImagemContainer}>
+            <img src={paqtg} id={styles.imgpaqtg} />
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Parceiros
+export default Parceiros;
